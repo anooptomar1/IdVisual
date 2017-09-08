895e65234ee7108b8695f60451aad0afee35a0e2
Minni K Ang <minni.ang@gmail.com>
2017-09-08 09:01:53 -0400

    UI added

---
+++ IdVisual/View/RoundedShadowImageView.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedShadowImageView.swift
+//  IdVisual
+//
+//  Created by Minni K Ang on 2017-09-08.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    override func awakeFromNib() {
+        self.layer.shadowColor = #colorLiteral(red: 0.4078193307, green: 0.4078193307, blue: 0.4078193307, alpha: 1)
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = 15
+    }
+
+}
